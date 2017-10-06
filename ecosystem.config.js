1f1bda91fbc1fb41a4decae9e84263f55fc71bdb
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-05 21:39:17 -0700

    eco

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
+        // First application
+        {
+            name: 'blackboard;',
+            script: 'bin/www',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+
+    ]
+
+
+};
