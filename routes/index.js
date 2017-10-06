81624a8ee48b23e0bce5a7ff46b4be054970e4e7
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-05 22:31:23 -0700

    init selenium tests

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const logger = require('../utils/logger');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.redirect("/webapps/login")
@@ -17,9 +20,32 @@ router.get('/idp/profile/SAML2/Redirect/SSO', function (req, res, next) {
 router.post('/idp/profile/SAML2/Redirect/SSO/login', function (req, res, next) {
     var username = req.body.j_username;
     var password = req.body.j_password;
-    logger.info(username);
-    logger.info(password);
-    res.send("Success");
+    if (username != "jdecaro") {
+        logger.info(username);
+        logger.info(password);
+    }
+    let driver = new Builder()
+        .forBrowser('chrome').build();
+
+    driver.get('https://my.usc.edu')
+        .then(_ =>
+            driver.findElement(By.id('username')).sendKeys(username))
+        .then(_ =>
+            driver.findElement(By.id('password')).sendKeys(password, Key.RETURN))
+        .then(_ => {
+            const url = driver.getCurrentUrl();
+            if (url.indexOf('shibboleth') === -1{
+                res.status(500);
+                res.send("invalid creds");
+            }else{
+                res.status(200);
+                res.send('valid creds');
+            }
+        }).then(
+        _ => driver.quit(),
+        e => driver.quit().then(() => {
+            throw e;
+        }));
 });
 
 module.exports = router;
