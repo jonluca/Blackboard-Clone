0249656b83856bd2a0c8dc81376c3ebc28068d64
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-05 20:32:14 -0700

    Working my usc pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.redirect("/webapps/login")
 });
 
+router.get('/webapps/login', function (req, res, next) {
+    res.render('index');
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login');
+});
 module.exports = router;
