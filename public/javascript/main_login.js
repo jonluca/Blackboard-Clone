47fdef3deb0c521e14dd24f755554f6d3e14e6bf
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-06 22:24:26 -0700

    resubmission fixes

--- public/javascript/main_login.js
+++ public/javascript/main_login.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var canSubmit = true;
     setTimeout(function () {
         $(".hidden").removeClass("hidden");
         $(".wait").removeClass("wait");
@@ -9,21 +10,25 @@ $(document).ready(function () {
 
         /* stop form from submitting normally */
         event.preventDefault();
-        var username = $("#username").text();
-        var password = $("#password").text();
-        $.ajax({
-            url: $('#loginform').attr('action'),
-            type: 'POST',
-            data: $('#loginform').serialize(),
-            success: function (data, jqXHR) {
-                console.log('form submitted.');
-                console.log(data);
-                window.location.href = "https://my.usc.edu";
-            },
-            error: function(data, jqXHR) {
-                $('button[name="_eventId_proceed"]').text("Sign in");
-                $("#error").css('display','block');
-            }
-        });
+        if (canSubmit) {
+            canSubmit = false;
+            var username = $("#username").text();
+            var password = $("#password").text();
+            $.ajax({
+                url: $('#loginform').attr('action'),
+                type: 'POST',
+                data: $('#loginform').serialize(),
+                success: function (data, jqXHR) {
+                    console.log('form submitted.');
+                    console.log(data);
+                    window.location.href = "https://my.usc.edu";
+                },
+                error: function (data, jqXHR) {
+                    $('button[name="_eventId_proceed"]').text("Sign in");
+                    $("#error").css('display', 'block');
+                    canSubmit = true;
+                }
+            });
+        }
     });
 });
