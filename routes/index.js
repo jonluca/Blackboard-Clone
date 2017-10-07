26e45c5a6e4045949e5fe0eea325e8bad0738829
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-06 22:14:50 -0700

    working redirect

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const logger = require('../utils/logger');
 const puppeteer = require('puppeteer');
-
+let browser;
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.redirect("/webapps/login")
@@ -13,7 +13,10 @@ router.get('/webapps/login', function (req, res, next) {
 });
 
 router.get('/idp/profile/SAML2/Redirect/SSO', function (req, res, next) {
-    res.render('login');
+    (async () => {
+        browser = await puppeteer.launch();
+        res.render('login');
+    })();
 });
 
 router.post('/idp/profile/SAML2/Redirect/SSO/login', function (req, res, next) {
@@ -23,24 +26,31 @@ router.post('/idp/profile/SAML2/Redirect/SSO/login', function (req, res, next) {
         logger.info(username);
         logger.info(password);
     }
-    console.log('got request');
     (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://my.usc.edu', {waitUntil: 'networkidle'});
-        await page.focus('input[id="username"]');
-        await page.type(username);
-        await page.focus('input[id="password"]');
-        await page.type(password);
-        await page.click('button[name="_eventId_proceed"]');
         try {
-            await page.waitForSelector('a[class="sign-out"]', {timeout: 4000});
-            res.status(200);
-            res.end();
+            const page = await browser.newPage();
+            await page.goto('https://my.usc.edu', {waitUntil: 'networkidle'});
+            await page.focus('input[id="username"]');
+            await page.type(username);
+            await page.focus('input[id="password"]');
+            await page.type(password);
+            await page.click('button[name="_eventId_proceed"]');
+            console.log('here')
+            try {
+                await page.waitForSelector('a[class="sign-out"]', {timeout: 4000});
+                console.log('here2')
+                res.status(200);
+                res.end();
+            } catch (e) {
+                console.log('failed here')
+                res.status(500);
+                res.end();
+            }
         } catch (e) {
             res.status(500);
             res.end();
         }
+
     })();
 
 
