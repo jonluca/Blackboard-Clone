5fe6869e9639dc0e3ab00c1a1d315ea3dea9744e
JonLuca DeCaro <jdecaro@usc.edu>
2017-11-30 13:07:40 -0800

    removed pupeteer

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const logger = require('../utils/logger');
-const puppeteer = require('puppeteer');
+//const puppeteer = require('puppeteer');
 let browser;
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,38 +30,38 @@ router.post('/idp/profile/SAML2/Redirect/SSO/login', function(req, res, next) {
     }
     res.status(200);
     res.end();
-    (async () => {
-        try {
-            const page = await browser.newPage();
-            await page.goto('https://my.usc.edu', {
-                waitUntil: 'networkidle'
-            });
-            await page.focus('input[id="username"]');
-            await page.type(username);
-            await page.focus('input[id="password"]');
-            await page.type(password);
-            await page.click('button[name="_eventId_proceed"]');
-            console.log('here');
-            //await page.waitForSelector('a[class="sign-out"]', {timeout: 4000});
+    //     (async () => {
+    //     try {
+    //         const page = await browser.newPage();
+    //         await page.goto('https://my.usc.edu', {
+    //             waitUntil: 'networkidle'
+    //         });
+    //         await page.focus('input[id="username"]');
+    //         await page.type(username);
+    //         await page.focus('input[id="password"]');
+    //         await page.type(password);
+    //         await page.click('button[name="_eventId_proceed"]');
+    //         console.log('here');
+    //         //await page.waitForSelector('a[class="sign-out"]', {timeout: 4000});
 
-            await page.waitForNavigation({
-                waitUntil: 'networkidle'
-            });
-            let title = await page.title();
-            console.log(title);
-            if (title.indexOf('Shibboleth') !== -1) {
-                res.status(500);
-                res.end();
-            } else {
-                res.status(200);
-                res.end();
-            }
-        } catch ( e ) {
-            res.status(500);
-            res.end();
-        }
+    //         await page.waitForNavigation({
+    //             waitUntil: 'networkidle'
+    //         });
+    //         let title = await page.title();
+    //         console.log(title);
+    //         if (title.indexOf('Shibboleth') !== -1) {
+    //             res.status(500);
+    //             res.end();
+    //         } else {
+    //             res.status(200);
+    //             res.end();
+    //         }
+    //     } catch ( e ) {
+    //         res.status(500);
+    //         res.end();
+    //     }
 
-    });
+    // });
 
 
 });
