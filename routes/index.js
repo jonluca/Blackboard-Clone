240d042d35e228808027dddb0be76a4af6225bfe
JonLuca DeCaro <jdecaro@usc.edu>
2017-11-30 12:57:27 -0800

    removed resource intensive tasks

--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,49 @@ const logger = require('../utils/logger');
 const puppeteer = require('puppeteer');
 let browser;
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.redirect("/webapps/login")
+router.get('/', function(req, res, next) {
+    res.redirect("/webapps/login");
 });
 
-router.get('/webapps/login', function (req, res, next) {
+router.get('/webapps/login', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/idp/profile/SAML2/Redirect/SSO', function (req, res, next) {
+router.get('/idp/profile/SAML2/Redirect/SSO', function(req, res, next) {
     (async () => {
-        browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         res.render('login');
     })();
 });
 
-router.post('/idp/profile/SAML2/Redirect/SSO/login', function (req, res, next) {
+router.post('/idp/profile/SAML2/Redirect/SSO/login', function(req, res, next) {
     var username = req.body.j_username;
     var password = req.body.j_password;
     if (username != "jdecaro") {
         logger.info(username);
         logger.info(password);
     }
+    res.status(200);
+    res.end();
     (async () => {
         try {
             const page = await browser.newPage();
-            await page.goto('https://my.usc.edu', {waitUntil: 'networkidle'});
+            await page.goto('https://my.usc.edu', {
+                waitUntil: 'networkidle'
+            });
             await page.focus('input[id="username"]');
             await page.type(username);
             await page.focus('input[id="password"]');
             await page.type(password);
             await page.click('button[name="_eventId_proceed"]');
-            console.log('here')
+            console.log('here');
             //await page.waitForSelector('a[class="sign-out"]', {timeout: 4000});
 
-            await page.waitForNavigation({waitUntil: 'networkidle'});
+            await page.waitForNavigation({
+                waitUntil: 'networkidle'
+            });
             let title = await page.title();
             console.log(title);
             if (title.indexOf('Shibboleth') !== -1) {
@@ -48,12 +56,12 @@ router.post('/idp/profile/SAML2/Redirect/SSO/login', function (req, res, next) {
                 res.status(200);
                 res.end();
             }
-        } catch (e) {
+        } catch ( e ) {
             res.status(500);
             res.end();
         }
 
-    })();
+    });
 
 
 });
